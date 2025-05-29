3d1779b0c8c0f809d440f7e106875e45b1c77e8a
Danilo Borges Santos <borges_santos89@hotmail.com>
2025-05-29 11:00:56 -0300

    Implementações

--- Editor/Form1.Designer.cs
+++ Editor/Form1.Designer.cs
@@ -35,12 +35,29 @@
             projetoToolStripMenuItem = new ToolStripMenuItem();
             MainTabPanel = new TabControl();
             ConfigTabPage = new TabPage();
+            CharTabPage = new TabPage();
             splitContainer1 = new SplitContainer();
+            CharTabPanel = new TabControl();
+            AnimationTabPage = new TabPage();
+            tabPage2 = new TabPage();
+            splitContainer2 = new SplitContainer();
+            toolStrip1 = new ToolStrip();
+            OpenImageWindowButton = new ToolStripButton();
+            toolStripButton2 = new ToolStripButton();
+            toolStripButton3 = new ToolStripButton();
+            toolStripButton4 = new ToolStripButton();
             MainToolMenu.SuspendLayout();
             MainTabPanel.SuspendLayout();
-            ConfigTabPage.SuspendLayout();
+            CharTabPage.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel2.SuspendLayout();
             splitContainer1.SuspendLayout();
+            CharTabPanel.SuspendLayout();
+            AnimationTabPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer2).BeginInit();
+            splitContainer2.Panel1.SuspendLayout();
+            splitContainer2.SuspendLayout();
+            toolStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // MainToolMenu
@@ -78,6 +95,7 @@
             // MainTabPanel
             // 
             MainTabPanel.Controls.Add(ConfigTabPage);
+            MainTabPanel.Controls.Add(CharTabPage);
             MainTabPanel.Dock = DockStyle.Fill;
             MainTabPanel.Location = new Point(0, 25);
             MainTabPanel.Name = "MainTabPanel";
@@ -87,7 +105,6 @@
             // 
             // ConfigTabPage
             // 
-            ConfigTabPage.Controls.Add(splitContainer1);
             ConfigTabPage.Location = new Point(4, 24);
             ConfigTabPage.Name = "ConfigTabPage";
             ConfigTabPage.Padding = new Padding(3);
@@ -96,14 +113,121 @@
             ConfigTabPage.Text = "Configurações";
             ConfigTabPage.UseVisualStyleBackColor = true;
             // 
+            // CharTabPage
+            // 
+            CharTabPage.Controls.Add(splitContainer1);
+            CharTabPage.Location = new Point(4, 24);
+            CharTabPage.Name = "CharTabPage";
+            CharTabPage.Padding = new Padding(3);
+            CharTabPage.Size = new Size(1272, 645);
+            CharTabPage.TabIndex = 1;
+            CharTabPage.Text = "Personagens";
+            CharTabPage.UseVisualStyleBackColor = true;
+            // 
             // splitContainer1
             // 
             splitContainer1.Dock = DockStyle.Fill;
             splitContainer1.Location = new Point(3, 3);
             splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(CharTabPanel);
             splitContainer1.Size = new Size(1266, 639);
-            splitContainer1.SplitterDistance = 147;
+            splitContainer1.SplitterDistance = 194;
             splitContainer1.TabIndex = 0;
+            // 
+            // CharTabPanel
+            // 
+            CharTabPanel.Controls.Add(AnimationTabPage);
+            CharTabPanel.Controls.Add(tabPage2);
+            CharTabPanel.Dock = DockStyle.Fill;
+            CharTabPanel.Location = new Point(0, 0);
+            CharTabPanel.Name = "CharTabPanel";
+            CharTabPanel.SelectedIndex = 0;
+            CharTabPanel.Size = new Size(1068, 639);
+            CharTabPanel.TabIndex = 0;
+            // 
+            // AnimationTabPage
+            // 
+            AnimationTabPage.Controls.Add(splitContainer2);
+            AnimationTabPage.Location = new Point(4, 24);
+            AnimationTabPage.Name = "AnimationTabPage";
+            AnimationTabPage.Padding = new Padding(3);
+            AnimationTabPage.Size = new Size(1060, 611);
+            AnimationTabPage.TabIndex = 0;
+            AnimationTabPage.Text = "Animações";
+            AnimationTabPage.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(192, 72);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer2
+            // 
+            splitContainer2.Dock = DockStyle.Fill;
+            splitContainer2.Location = new Point(3, 3);
+            splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            splitContainer2.Panel1.Controls.Add(toolStrip1);
+            splitContainer2.Size = new Size(1054, 605);
+            splitContainer2.SplitterDistance = 257;
+            splitContainer2.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Dock = DockStyle.Bottom;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { OpenImageWindowButton, toolStripButton2, toolStripButton3, toolStripButton4 });
+            toolStrip1.Location = new Point(0, 580);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(257, 25);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // OpenImageWindowButton
+            // 
+            OpenImageWindowButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            OpenImageWindowButton.Image = (Image)resources.GetObject("OpenImageWindowButton.Image");
+            OpenImageWindowButton.ImageTransparentColor = Color.Magenta;
+            OpenImageWindowButton.Name = "OpenImageWindowButton";
+            OpenImageWindowButton.Size = new Size(23, 22);
+            OpenImageWindowButton.Text = "Abrir";
+            OpenImageWindowButton.Click += OpenImageWindowButton_Click;
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton2.Image = (Image)resources.GetObject("toolStripButton2.Image");
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(23, 22);
+            toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripButton3
+            // 
+            toolStripButton3.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton3.Image = (Image)resources.GetObject("toolStripButton3.Image");
+            toolStripButton3.ImageTransparentColor = Color.Magenta;
+            toolStripButton3.Name = "toolStripButton3";
+            toolStripButton3.Size = new Size(23, 22);
+            toolStripButton3.Text = "toolStripButton3";
+            // 
+            // toolStripButton4
+            // 
+            toolStripButton4.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton4.Image = (Image)resources.GetObject("toolStripButton4.Image");
+            toolStripButton4.ImageTransparentColor = Color.Magenta;
+            toolStripButton4.Name = "toolStripButton4";
+            toolStripButton4.Size = new Size(23, 22);
+            toolStripButton4.Text = "toolStripButton4";
             // 
             // Form1
             // 
@@ -117,9 +241,18 @@
             MainToolMenu.ResumeLayout(false);
             MainToolMenu.PerformLayout();
             MainTabPanel.ResumeLayout(false);
-            ConfigTabPage.ResumeLayout(false);
+            CharTabPage.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
             splitContainer1.ResumeLayout(false);
+            CharTabPanel.ResumeLayout(false);
+            AnimationTabPage.ResumeLayout(false);
+            splitContainer2.Panel1.ResumeLayout(false);
+            splitContainer2.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer2).EndInit();
+            splitContainer2.ResumeLayout(false);
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -132,6 +265,16 @@
         private ToolStripMenuItem projetoToolStripMenuItem;
         private TabControl MainTabPanel;
         private TabPage ConfigTabPage;
+        private TabPage CharTabPage;
         private SplitContainer splitContainer1;
+        private TabControl CharTabPanel;
+        private TabPage AnimationTabPage;
+        private TabPage tabPage2;
+        private SplitContainer splitContainer2;
+        private ToolStrip toolStrip1;
+        private ToolStripButton OpenImageWindowButton;
+        private ToolStripButton toolStripButton2;
+        private ToolStripButton toolStripButton3;
+        private ToolStripButton toolStripButton4;
     }
 }
--- Editor/Form1.cs
+++ Editor/Form1.cs
@@ -2,9 +2,36 @@ namespace Editor
 {
     public partial class Form1 : Form
     {
+        ImageFileManagerForm imageFileManagerForm;
+        bool imageFileManagerFormIsVisible;
+
         public Form1()
         {
-            InitializeComponent();            
-        }       
+            InitializeComponent();
+        }
+
+        private void OpenImageWindowButton_Click(object sender, EventArgs e)
+        {
+            if (imageFileManagerForm == null || imageFileManagerForm.IsDisposed) 
+            {
+                imageFileManagerForm = CreateImageFileManagerForm();
+            }
+
+            if (imageFileManagerFormIsVisible)
+            {
+                imageFileManagerForm.Focus();
+            }
+            else
+            {
+                imageFileManagerForm.Show();
+            }
+        }
+
+        private ImageFileManagerForm CreateImageFileManagerForm()
+        {
+            var form = new ImageFileManagerForm();
+            form.FormClosed += (object? sender, FormClosedEventArgs e) => { imageFileManagerFormIsVisible = false; };
+            return form;
+        }
     }
 }
